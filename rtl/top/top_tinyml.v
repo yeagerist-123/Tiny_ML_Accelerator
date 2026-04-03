91dcfa7f8ff21b047da85b72b1c22800e058533b
yeagerist-123 <koushik.pedaprolu@gmail.com>
2026-04-03 16:05:28 +0530

    Update top_tinyml.v

--- rtl/top/top_tinyml.v
+++ rtl/top/top_tinyml.v
@@ -10,46 +10,26 @@ module top_tinyml (
     wire [127:0] w_bus;
     wire [31:0]  a_bus;
     wire [95:0]  psum_bus;
-    wire load_weights;
 
     controller_fsm fsm (
-        .clk(clk), .reset(reset), .start(start),
-        .load_weights(load_weights),
-        .done(done)
+        .clk(clk), .reset(reset), .start(start), .done(done)
     );
 
     weight_buffer wb (
-        .clk(clk),
-        .write_en(wr_weight_en),
-        .addr(addr),
-        .data_in(data_in),
-        .weight_bus(w_bus)
+        .clk(clk), .reset(reset), .write_en(wr_weight_en), .addr(addr), .data_in(data_in), .weight_bus(w_bus)
     );
 
     activation_buffer ab (
-        .clk(clk),
-        .write_en(wr_act_en),
-        .addr(addr[1:0]),
-        .data_in(data_in),
-        .act_bus(a_bus)
+        .clk(clk), .reset(reset), .write_en(wr_act_en), .addr(addr[1:0]), .data_in(data_in), .act_bus(a_bus)
     );
 
     systolic_array sa (
-        .clk(clk),
-        .reset(reset),
-        .load_weights(load_weights),
-        .weight_bus(w_bus),
-        .act_in_bus(a_bus),
-        .psum_out_bus(psum_bus)
+        .weight_bus(w_bus), .act_in_bus(a_bus), .psum_out_bus(psum_bus)
     );
 
-    wire [7:0] o0,o1,o2,o3;
-
-    relu r0(.data_in(psum_bus[23:0]), .data_out(o0));
-    relu r1(.data_in(psum_bus[47:24]), .data_out(o1));
-    relu r2(.data_in(psum_bus[71:48]), .data_out(o2));
-    relu r3(.data_in(psum_bus[95:72]), .data_out(o3));
-
-    assign ml_output = {o3,o2,o1,o0};
+    relu r0(.data_in(psum_bus[23:0]),  .data_out(ml_output[7:0]));
+    relu r1(.data_in(psum_bus[47:24]), .data_out(ml_output[15:8]));
+    relu r2(.data_in(psum_bus[71:48]), .data_out(ml_output[23:16]));
+    relu r3(.data_in(psum_bus[95:72]), .data_out(ml_output[31:24]));
 
 endmodule
